f3ac8c051951c774607436743734b4259e4d357b
Tanglinfeng <352696800@qq.com>
2022-05-26 15:29:37 +0800

    feat: 个人分数接口

--- course-service-parent/course-service/src/main/java/com/af/course/controller/ScoreController.java
+++ course-service-parent/course-service/src/main/java/com/af/course/controller/ScoreController.java
@@ -1,15 +1,15 @@
 package com.af.course.controller;
 
+import com.af.common.constant.TokenConstants;
 import com.af.common.model.ResponseBean;
+import com.af.common.utils.JwtUtil;
 import com.af.course.api.vo.StudentScoreVo;
+import com.af.course.service.QuestionSubmitService;
 import com.af.course.service.ScoreService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,9 +25,18 @@ public class ScoreController {
 
     private final ScoreService scoreService;
 
+    private final QuestionSubmitService questionSubmitService;
+
     @ApiOperation("获取分数前10名")
     @GetMapping("/getTop10")
     public ResponseBean<List<StudentScoreVo>> getTop10Student(@RequestParam("courseId") String courseId) {
         return new ResponseBean<>(scoreService.showScore(courseId));
+    }
+
+    @ApiOperation("获取自己的分数")
+    @GetMapping("/getMyScore")
+    public ResponseBean<Integer> myScore(@RequestParam("courseId") String courseId, @RequestHeader(TokenConstants.TOKEN_HEADER) String token) {
+        Long userId = JwtUtil.getUserId(token);
+        return new ResponseBean<>(questionSubmitService.getUserScore(userId.toString(), courseId));
     }
 }
